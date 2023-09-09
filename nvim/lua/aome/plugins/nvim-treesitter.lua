1e7959fefa4ebdb47937630c9ac57b501ab82dc9
AlejandroSuero <71392160+AlejandroSuero@users.noreply.github.com>
2023-09-09 23:20:25 +0200

    feat(nvim): Changed config

--- nvim/lua/aome/plugins/nvim-treesitter.lua
+++ nvim/lua/aome/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
         "windwp/nvim-ts-autotag",
         "nvim-treesitter/playground",
-        "nvim-treesitter/nvim-treesitter-context"
+        "nvim-treesitter/nvim-treesitter-context",
     },
     config = function()
         local ok, treesitter = pcall(require, "nvim-treesitter.configs")
@@ -15,44 +15,40 @@ return {
 
         treesitter.setup({
             highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = { enable = true },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "json",
-          "javascript",
-          "typescript",
-          "tsx",
-          "yaml",
-          "html",
-          "css",
-          "prisma",
-          "markdown",
-          "markdown_inline",
-          "svelte",
-          "graphql",
-          "bash",
-          "lua",
-          "vim",
-          "vimdoc",
-          "dockerfile",
-          "gitignore",
-          "php",
-          "go",
-          "rust",
-          "c"
-        },
-        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
-        -- auto install above language parsers
-        auto_install = true,
+                enable = true,
+            },
+            indent = { enable = true },
+            autotag = { enable = true },
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "prisma",
+                "markdown",
+                "markdown_inline",
+                "svelte",
+                "graphql",
+                "bash",
+                "lua",
+                "vim",
+                "vimdoc",
+                "dockerfile",
+                "gitignore",
+                "php",
+                "go",
+                "rust",
+                "c",
+                "astro",
+            },
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+            auto_install = true,
         })
-    end
+    end,
 }
