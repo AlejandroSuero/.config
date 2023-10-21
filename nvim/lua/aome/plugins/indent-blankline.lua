fab9ad80a1eeaf507ee52090d4bb3fc80b71c0f7
AlejandroSuero <asuerome@gmail.com>
2023-10-21 18:43:53 +0200

    feat(nvim): Migrated to indent-blankline v3

--- nvim/lua/aome/plugins/indent-blankline.lua
+++ nvim/lua/aome/plugins/indent-blankline.lua
@@ -18,26 +18,27 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     lazy = true,
     event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
     opts = {
-        indentLine_enabled = 1,
-        filetype_exclude = {
-            "help",
-            "terminal",
-            "lazy",
-            "lspinfo",
-            "TelescopePrompt",
-            "TelescopeResults",
-            "mason",
-            "",
+        exclude = {
+            filetypes = {
+                "help",
+                "terminal",
+                "lazy",
+                "lspinfo",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "mason",
+                "",
+            },
+            buftypes = { "terminal" },
         },
-        buftype_exclude = { "terminal" },
-        show_trailing_blankline_indent = false,
-        show_first_indent_level = true,
-        show_current_context = true,
-        show_current_context_start = true,
+        scope = {
+            enabled = true,
+        },
     },
     config = function(_, opts)
-        local ok_blankline, blankline = pcall(require, "indent_blankline")
+        local ok_blankline, blankline = pcall(require, "ibl")
         if not ok_blankline then
             vim.notify("Indent blankline not installed", 3)
             return
