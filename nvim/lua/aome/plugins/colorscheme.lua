6b8d0c2ede70c93d8029e5eb556ddeec6cdb0779
AlejandroSuero <asuerome@gmail.com>
2023-10-30 16:51:34 +0100

    feat(nvim): Refactoring plugins

--- nvim/lua/aome/plugins/colorscheme.lua
+++ nvim/lua/aome/plugins/colorscheme.lua
@@ -4,92 +4,95 @@ return {
         priority = 1000,
         lazy = false,
         name = "catppuccin",
-        config = function()
+        opts = {
+            transparent_background = vim.g.transparency,
+            term_colors = true,
+            dim_inactive = {
+                enabled = true,
+                percentage = 0.5,
+            },
+            highliight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            styles = {
+                conditionals = {},
+            },
+            integrations = {
+                treesitter = true,
+                native_lsp = {
+                    enable = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        information = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        information = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                    },
+                },
+                nvimtree = true,
+                noice = true,
+                notify = true,
+                lsp_trouble = false,
+                cmp = true,
+                gitsigns = true,
+                telescope = {
+                    enabled = true,
+                    style = "nvchad",
+                },
+                markdown = true,
+                mason = true,
+                harpoon = true,
+                bufferline = true,
+            },
+            highlight_overrides = {
+                all = function(_)
+                    local colors = require("aome.colorschemes.catppuccin")
+                    return {
+                        CursorLine = { bg = colors.darker_black },
+                        ColorColumn = { bg = colors.darker_black },
+                    }
+                end,
+            },
+            compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+        },
+        config = function(_, opts)
             local ok, catppuccin = pcall(require, "catppuccin")
             if not ok then
                 vim.notify("COLORSCHEME: catppuccin is not installed", 3)
                 return
             end
 
-            catppuccin.setup({
-                transparent_background = vim.g.transparency,
-                term_colors = true,
-                dim_inactive = {
-                    enabled = true,
-                    percentage = 0.5,
-                },
-                highliight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                styles = {
-                    conditionals = {},
-                },
-                integrations = {
-                    treesitter = true,
-                    native_lsp = {
-                        enable = true,
-                        virtual_text = {
-                            errors = { "italic" },
-                            information = { "italic" },
-                            hints = { "italic" },
-                            warnings = { "italic" },
-                        },
-                        underlines = {
-                            errors = { "underline" },
-                            information = { "underline" },
-                            hints = { "underline" },
-                            warnings = { "underline" },
-                        },
-                    },
-                    nvimtree = true,
-                    noice = true,
-                    notify = true,
-                    lsp_trouble = false,
-                    cmp = true,
-                    gitsigns = true,
-                    telescope = {
-                        enabled = true,
-                        style = "nvchad",
-                    },
-                    markdown = true,
-                    mason = true,
-                    harpoon = true,
-                    bufferline = true,
-                },
-                highlight_overrides = {
-                    all = function(_)
-                        local colors = require("aome.colorschemes.catppuccin")
-                        return {
-                            CursorLine = { bg = colors.darker_black },
-                            ColorColumn = { bg = colors.darker_black },
-                        }
-                    end,
-                },
-                compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-            })
+            catppuccin.setup(opts)
             vim.cmd.colorscheme(vim.g.colorscheme)
         end,
     },
     {
         "rose-pine/neovim",
         name = "rose-pine",
-        config = function()
+        lazy = true,
+        opts = {
+            disable_background = vim.g.transparency,
+        },
+        config = function(_, opts)
             local ok, rosepine = pcall(require, "rose-pine")
             if not ok then
                 vim.notify("COLORSCHEME: rose-pine not installed", 3)
                 return
             end
 
-            rosepine.setup({
-                disable_background = vim.g.transparency,
-            })
+            rosepine.setup(opts)
             vim.cmd.colorscheme(vim.g.colorscheme)
         end,
     },
     {
         "folke/tokyonight.nvim",
-        lazy = false,
+        lazy = true,
         opts = {
             style = "night",
             light_style = "storm",
@@ -111,6 +114,7 @@ return {
                 return
             end
             tokyonight.setup(opts)
+            vim.cmd.colorscheme(vim.g.colorscheme)
         end,
     },
 }
