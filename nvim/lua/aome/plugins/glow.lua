a74871f2f5901ffd2c9e38032565d6ead86f1861
AlejandroSuero <asuerome@gmail.com>
2023-11-02 08:08:39 +0100

    feat(nvim): Added markdown previewer

---
+++ nvim/lua/aome/plugins/glow.lua
@@ -0,0 +1,21 @@
+return {
+    "ellisonleao/glow.nvim",
+    lazy = true,
+    config = function()
+        local ok, glow = pcall(require, "glow")
+        if not ok then
+            vim.notify("Glow is not installed", vim.log.levels.ERROR)
+        end
+        glow.setup({
+            install_path = "~/.local/bin",
+            style = "dark",
+            width = 120,
+            height = 150,
+        })
+    end,
+    cmd = "Glow",
+    keys = {
+        { "<leader>md", "<cmd>Glow<cr>", desc = "Markdown Preview" },
+    },
+    ft = "markdown",
+}
