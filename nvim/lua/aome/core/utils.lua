88581c42747cea08f1ba137775cce881b5fca641
AlejandroSuero <asuerome@gmail.com>
2023-11-03 22:26:46 +0100

    feat(nvim): cleaning up plugins

--- nvim/lua/aome/core/utils.lua
+++ nvim/lua/aome/core/utils.lua
@@ -6,26 +6,23 @@ local M = {}
 ---@param mappings_table table Table that contains the mappings
 ---@param mapping_opt table|nil that contains the mapping options
 M.map_keys = function(mappings_table, mapping_opt)
-    for mode, mappings in pairs(mappings_table) do
-        local default_opts =
-            vim.tbl_deep_extend("force", { mode = mode }, mapping_opt or {})
+  for mode, mappings in pairs(mappings_table) do
+    local default_opts =
+      vim.tbl_deep_extend("force", { mode = mode }, mapping_opt or {})
 
-        for keybind, mapping_info in pairs(mappings) do
-            if type(mapping_info) ~= "table" then
-                mapping_info = { mapping_info }
-            end
-            local opts = vim.tbl_deep_extend(
-                "force",
-                default_opts,
-                mapping_info.opts or {}
-            )
+    for keybind, mapping_info in pairs(mappings) do
+      if type(mapping_info) ~= "table" then
+        mapping_info = { mapping_info }
+      end
+      local opts =
+        vim.tbl_deep_extend("force", default_opts, mapping_info.opts or {})
 
-            mapping_info.opts, opts.mode = nil, nil
-            opts.desc = mapping_info[2] or "No description added"
+      mapping_info.opts, opts.mode = nil, nil
+      opts.desc = mapping_info[2] or "No description added"
 
-            vim.keymap.set(mode, keybind, mapping_info[1], opts)
-        end
+      vim.keymap.set(mode, keybind, mapping_info[1], opts)
     end
+  end
 end
 
 ---@class default_coloscheme_opts
@@ -33,68 +30,61 @@ end
 ---@field theme string
 ---@field transparency boolean
 M.default_coloscheme_opts = {
-    base = "base46",
-    theme = vim.g.colorscheme,
-    transparency = vim.g.transparency,
+  base = "base46",
+  theme = vim.g.colorscheme,
+  transparency = vim.g.transparency,
 }
 
 --- Replace old colorscheme for the new one
 ---@param old string Old colorscheme you want to change
 ---@param new string New colorscheme you want to change
 M.replace_word = function(old, new)
-    local options = vim.fn.stdpath("config")
-        .. "/lua/aome/core/"
-        .. "colorscheme.lua"
-    local file = io.open(options, "r")
-    local added_pattern = string.gsub(old, "-", "%%-") -- add % before - if exists
-    print("added_pattern: " .. added_pattern)
-    print("new: " .. new)
-    print("old: " .. old)
-    if file == nil then
-        vim.notify("Config file not found", vim.log.levels.ERROR)
-        return
-    end
+  local options = vim.fn.stdpath("config")
+    .. "/lua/aome/core/"
+    .. "colorscheme.lua"
+  local file = io.open(options, "r")
+  local added_pattern = string.gsub(old, "-", "%%-") -- add % before - if exists
+  if file then
     local new_content = file:read("*all"):gsub(added_pattern, new)
-    print("new_content: " .. new_content)
-
     file = io.open(options, "w")
     file:write(new_content)
     file:close()
+  end
 end
 
 --- List all the themes in the base46/hl_themes directory and self/colorscheme directory
 ---@return string[] default_themes Names of all the configured themes
 M.list_themes = function()
-    local default_themes = vim.fn.readdir(
-        vim.fn.stdpath("data") .. "/lazy/base46/lua/base46/hl_themes"
-    )
+  local default_themes = vim.fn.readdir(
+    vim.fn.stdpath("data") .. "/lazy/base46/lua/base46/hl_themes"
+  )
 
-    local custom_themes =
-        vim.loop.fs_stat(vim.fn.stdpath("config") .. "/lua/colorschemes")
+  local custom_themes =
+    vim.loop.fs_stat(vim.fn.stdpath("config") .. "/lua/colorschemes")
 
-    if custom_themes and custom_themes.type == "directory" then
-        local themes_tb =
-            vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/colorschemes")
-        for _, value in ipairs(themes_tb) do
-            default_themes[#default_themes + 1] = value
-        end
+  if custom_themes and custom_themes.type == "directory" then
+    local themes_tb =
+      vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/colorschemes")
+    for _, value in ipairs(themes_tb) do
+      default_themes[#default_themes + 1] = value
     end
+  end
 
-    for index, theme in ipairs(default_themes) do
-        default_themes[index] = theme:match("(.+)%..+")
-    end
+  for index, theme in ipairs(default_themes) do
+    default_themes[index] = theme:match("(.+)%..+")
+  end
 
-    return default_themes
+  return default_themes
 end
 
 --- Reloads the selected colorscheme
 ---@param colorscheme string Name of the colorscheme you want to reload
 M.reload_colorscheme = function(colorscheme)
-    vim.g.colorscheme = colorscheme
-    require("base46").load_theme({
-        theme = colorscheme,
-        transparency = vim.g.transparency,
-    })
+  vim.g.colorscheme = colorscheme
+  require("base46").load_theme({
+    theme = colorscheme,
+    transparency = vim.g.transparency,
+  })
 end
 
 return M
--- nvim/lua/aome/plugins/ui/telescope.lua
+++ nvim/lua/aome/plugins/ui/telescope.lua
@@ -1,171 +1,161 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-        "AlejandroSuero/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "AlejandroSuero/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = true,
+  cmd = {
+    "Telescope",
+  },
+  keys = {
+    {
+      "<leader>pf",
+      "<cmd>Telescope find_files<cr>",
+      desc = "Find project files",
     },
-    lazy = true,
-    cmd = {
-        "Telescope",
+    { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+    {
+      "<leader>th",
+      "<cmd>Telescope themes<cr>",
+      desc = "Change base46 colorscheme",
     },
-    config = function()
-        local ok, telescope = pcall(require, "telescope")
-        if not ok then
-            vim.notify("Telescope not loaded", 3)
-        end
+    {
+      "<leader>cs",
+      "<cmd>Telescope colorschemes<cr>",
+      desc = "Change colorscheme",
+    },
+    { "<leader>km", "<cmd>Telescope keymaps<cr>", desc = "Show keymaps" },
+    { "<leader>ht", "<cmd>Telescope help_tags<cr>", desc = "Show help tags" },
+    { "<leader>cm", "<cmd>Telescope commands<cr>", desc = "Show commands" },
+  },
+  config = function()
+    local ok, telescope = pcall(require, "telescope")
+    if not ok then
+      vim.notify("Telescope not loaded", 3)
+    end
 
-        local actions = require("telescope.actions")
+    local actions = require("telescope.actions")
 
-        telescope.setup({
-            defaults = {
-                mappings = {
-                    i = {
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-t>"] = actions.select_tab,
-                        ["<C-q>"] = actions.send_selected_to_qflist
-                            + actions.open_qflist,
-                    },
-                    n = {
-                        ["q"] = actions.close,
-                    },
-                },
-                file_ignore_patterns = {
-                    "node_modules/",
-                    "%.git/",
-                    "%.DS_Store$",
-                    "target/",
-                    "build/",
-                    "%.o$",
-                },
-                vimgrep_arguments = {
-                    "rg",
-                    "--color=never",
-                    "--no-heading",
-                    "--with-filename",
-                    "--line-number",
-                    "--column",
-                    "--smart-case",
-                    "--hidden",
-                },
-                layout_config = {
-                    prompt_position = "top",
-                    horizontal = {
-                        width_padding = 0.1,
-                        height_padding = 0.1,
-                        preview_width = 0.6,
-                    },
-                    vertical = {
-                        width_padding = 0.1,
-                        height_padding = 0.1,
-                        preview_height = 0.5,
-                    },
-                },
-                path_display = { "smart" },
-                prompt_position = "top",
-                prompt_prefix = " 󰍉 ",
-                selection_caret = "  ",
-                sorting_strategy = "ascending",
-                winblend = 0,
-                set_env = { ["COLORTERM"] = "truecolor" },
-                border = {},
-                borderchars = {
-                    "─",
-                    "│",
-                    "─",
-                    "│",
-                    "╭",
-                    "╮",
-                    "╯",
-                    "╰",
-                },
-                color_devicons = true,
-            },
-            pickers = {
-                colorscheme = {
-                    prompt_prefix = " 󰏘 ",
-                },
-                find_files = {
-                    prompt_prefix = " 󰈔 ",
-                    find_command = { "rg", "--files", "--hidden" },
-                },
-                git_files = {
-                    prompt_prefix = " 󰊢 ",
-                    show_untracked = true,
-                },
-                keymaps = {
-                    prompt_prefix = " 󰌌 ",
-                },
-                commands = {
-                    prompt_prefix = "  ",
-                },
-                help_tags = {
-                    prompt_prefix = " 󰞋 ",
-                },
-                grep_string = {
-                    prompt_prefix = " 󰬶 ",
-                },
-            },
-            extensions = {
-                fzf = {
-                    fuzzy = true, -- false will only do exact matching
-                    override_generic_sorter = true, -- override the generic sorter
-                    override_file_sorter = true, -- override the file sorter
-                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-                },
-            },
-        })
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-t>"] = actions.select_tab,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+          n = {
+            ["q"] = actions.close,
+          },
+        },
+        file_ignore_patterns = {
+          "node_modules/",
+          "%.git/",
+          "%.DS_Store$",
+          "target/",
+          "build/",
+          "%.o$",
+        },
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+        },
+        layout_config = {
+          prompt_position = "top",
+          horizontal = {
+            width_padding = 0.1,
+            height_padding = 0.1,
+            preview_width = 0.6,
+          },
+          vertical = {
+            width_padding = 0.1,
+            height_padding = 0.1,
+            preview_height = 0.5,
+          },
+        },
+        path_display = { "smart" },
+        prompt_position = "top",
+        prompt_prefix = " 󰍉 ",
+        selection_caret = "  ",
+        sorting_strategy = "ascending",
+        winblend = 0,
+        set_env = { ["COLORTERM"] = "truecolor" },
+        border = {},
+        borderchars = {
+          "─",
+          "│",
+          "─",
+          "│",
+          "╭",
+          "╮",
+          "╯",
+          "╰",
+        },
+        color_devicons = true,
+      },
+      pickers = {
+        colorscheme = {
+          prompt_prefix = " 󰏘 ",
+        },
+        find_files = {
+          prompt_prefix = " 󰈔 ",
+          find_command = { "rg", "--files", "--ignore", "--hidden" },
+        },
+        git_files = {
+          prompt_prefix = " 󰊢 ",
+          show_untracked = true,
+        },
+        keymaps = {
+          prompt_prefix = " 󰌌 ",
+        },
+        commands = {
+          prompt_prefix = "  ",
+        },
+        help_tags = {
+          prompt_prefix = " 󰞋 ",
+        },
+        grep_string = {
+          prompt_prefix = " 󰬶 ",
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true, -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true, -- override the file sorter
+          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        },
+      },
+    })
 
-        telescope.load_extension("harpoon")
-        telescope.load_extension("themes")
+    telescope.load_extension("harpoon")
+    telescope.load_extension("themes")
 
-        local builtin = require("telescope.builtin")
+    local builtin = require("telescope.builtin")
 
-        local mappings = {
-            n = {
-                -- project files
-                ["<leader>pf"] = {
-                    builtin.find_files,
-                    "Telescope find files",
-                },
-                -- git files
-                ["<C-p>"] = {
-                    builtin.git_files,
-                    "Telescope find git files",
-                },
-                -- colorschemes
-                ["<leader>th"] = {
-                    "<cmd>Telescope themes<cr>",
-                    "Telescope show colorschemes",
-                },
-                -- find string
-                ["<leader>ps"] = {
-                    function()
-                        builtin.grep_string({
-                            search = vim.fn.input("Grep -> "),
-                        })
-                    end,
-                    "Telescope grep string",
-                },
-                -- keymaps
-                ["<leader>km"] = {
-                    builtin.keymaps,
-                    "Telescope show keymaps",
-                },
-                -- help tags
-                ["<leader>ht"] = {
-                    builtin.help_tags,
-                    "Telescope show help tags",
-                },
-                -- commands
-                ["<A-p>"] = {
-                    builtin.commands,
-                    "Telescope show commands",
-                },
-            },
-        }
+    local mappings = {
+      n = {
+        -- find string
+        ["<leader>ps"] = {
+          function()
+            builtin.grep_string({
+              search = vim.fn.input("Grep -> "),
+            })
+          end,
+          "Telescope grep string",
+        },
+      },
+    }
 
-        require("aome.core.utils").map_keys(mappings)
-    end,
+    require("aome.core.utils").map_keys(mappings)
+  end,
 }
