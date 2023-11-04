e201d54238c2961de81ad4ea2aaab62766e8d62b
AlejandroSuero <asuerome@gmail.com>
2023-11-04 19:42:35 +0100

    feat(nvim): Modified for reloading a theme, sorten mapping

--- nvim/lua/aome/plugins/ui/zen.lua
+++ nvim/lua/aome/plugins/ui/zen.lua
@@ -1,36 +1,35 @@
----@diagnostic disable: undefined-global
+local reload_colorscheme = require("aome.core.utils").reload_colorscheme
+
 return {
-    "folke/zen-mode.nvim",
-    lazy = true,
-    opts = {
-        window = {
-            width = math.floor((vim.o.columns / 2) + 20),
-            backdrop = 0.50,
-            options = {
-                cursorline = true,
-            },
-            plugins = {
-                gitsigns = { enabled = false },
-                options = {
-                    laststatus = 3,
-                },
-            },
+  "folke/zen-mode.nvim",
+  lazy = true,
+  opts = {
+    window = {
+      width = math.floor((vim.o.columns / 2) + 20),
+      backdrop = 0.50,
+      options = {
+        cursorline = true,
+      },
+      plugins = {
+        gitsigns = { enabled = false },
+        options = {
+          laststatus = 3,
         },
-        on_open = function(win)
-            local colorscheme = "rose-pine-main"
-            vim.cmd.colorscheme(colorscheme)
-            vim.cmd.TransparentDisable()
-        end,
-        on_close = function()
-            vim.cmd.colorscheme(vim.g.colorscheme)
-        end,
+      },
     },
-    config = function(_, opts)
-        local zen_mode = require("zen-mode")
-        zen_mode.setup(opts)
+    on_open = function(_)
+      reload_colorscheme("catppuccin", "colorscheme")
     end,
-    cmd = "ZenMode",
-    keys = {
-        { "<leader>zz", "<cmd>ZenMode<cr>", desc = "Enter zen mode" },
-    },
+    on_close = function()
+      reload_colorscheme(vim.g.base46, "base46")
+    end,
+  },
+  config = function(_, opts)
+    local zen_mode = require("zen-mode")
+    zen_mode.setup(opts)
+  end,
+  cmd = "ZenMode",
+  keys = {
+    { "<leader>z", "<cmd>ZenMode<cr>", desc = "Enter zen mode" },
+  },
 }
