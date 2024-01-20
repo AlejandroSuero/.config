9897f08fab675f7437033c08437ed5b9910bc195
Alejandro Suero <asuerome@gmail.com>
2024-01-20 16:46:39 +0100

    Fixed colorscheme and base46 bug

--- nvim/lua/aome/plugins/ui/base46.lua
+++ nvim/lua/aome/plugins/ui/base46.lua
@@ -1,7 +1,6 @@
 return {
-  "jayden-chan/base46.nvim",
+  "AlejandroSuero/base46.nvim",
   lazy = false,
-  dir = os.getenv("HOME") .. "/dev/nvim_plugins/base46.nvim",
   branch = "fix-transparent",
   priority = 1000,
 }
