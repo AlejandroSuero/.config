8ec65c49678343aa8f696cf0fa72a6cde429fe59
Alejandro Suero <asuerome@gmail.com>
2024-01-20 19:20:19 +0100

    feat(nvim): Added ft config for C

---
+++ nvim/after/ftplugin/c.lua
@@ -0,0 +1,14 @@
+local options = {
+  bo = {
+    tabstop = 2,
+    softtabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+  },
+}
+
+for scope, opts in pairs(options) do
+  for opt, value in pairs(opts) do
+    vim[scope][opt] = value
+  end
+end
