f1913c1debad55166116076d1dad8e096b156e30
Alejandro Suero <asuerome@gmail.com>
2024-01-21 20:39:06 +0100

    feat(nvim): Adjusted to new treesitter config

--- nvim/lua/aome/plugins/nvim-treesitter.lua
+++ nvim/lua/aome/plugins/nvim-treesitter.lua
@@ -1,59 +1,69 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "windwp/nvim-ts-autotag",
-        "nvim-treesitter/playground",
-        "nvim-treesitter/nvim-treesitter-context",
-    },
-    config = function()
-        local ok, treesitter = pcall(require, "nvim-treesitter.configs")
-        if not ok then
-            vim.notify("Treesitter not loaded", 3)
-            return
-        end
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    "nvim-treesitter/playground",
+    "nvim-treesitter/nvim-treesitter-context",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  config = function()
+    local ok, treesitter = pcall(require, "nvim-treesitter.configs")
+    if not ok then
+      vim.notify("Treesitter not loaded", 3)
+      return
+    end
 
-        treesitter.setup({
-            highlight = {
-                enable = true,
-            },
-            indent = { enable = true },
-            autotag = {
-                enable = true,
-                enable_rename = true,
-                enable_close = true,
-                enable_close_on_slash = true,
-            },
-            ensure_installed = {
-                "json",
-                "javascript",
-                "typescript",
-                "tsx",
-                "yaml",
-                "html",
-                "css",
-                "prisma",
-                "markdown",
-                "markdown_inline",
-                "svelte",
-                "graphql",
-                "bash",
-                "lua",
-                "vim",
-                "vimdoc",
-                "dockerfile",
-                "gitignore",
-                "php",
-                "go",
-                "rust",
-                "c",
-                "astro",
-            },
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            },
-            auto_install = true,
-        })
-    end,
+    treesitter.setup {
+      highlight = {
+        enable = true,
+      },
+      indent = { enable = true },
+      autotag = {
+        enable = true,
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+      },
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "vimdoc",
+        "dockerfile",
+        "gitignore",
+        "php",
+        "go",
+        "rust",
+        "c",
+        "astro",
+      },
+      auto_install = true,
+    }
+
+    local ok2, ts_context_commentstring =
+      pcall(require, "ts_context_commentstring")
+    if not ok2 then
+      vim.notify("Tresitter context comment string not loaded", 3)
+      return
+    end
+    ts_context_commentstring.setup {
+      enable_autocmd = false,
+      languages = {
+        typescript = "// %s",
+      },
+    }
+  end,
 }
