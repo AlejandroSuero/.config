e3e5d5a9b611e8fe8b9e06c190c0972c54200216
Alejandro Suero <aome@Alejandros-MacBook-Pro.local>
2024-03-04 20:51:38 +0100

    feat(nvim): Refactored fugitive options

--- nvim/after/ftplugin/fugitive.lua
+++ nvim/after/ftplugin/fugitive.lua
@@ -1,49 +1,50 @@
 local select_opts = { "Commit", "Pull", "Push", "Push -u", "Fetch" }
-local select = function()
-  vim.ui.select(select_opts, {
-    prompt = "Vim-Fugitive options",
-    format_item = function(item)
-      if item == "Push -u" then
-        return item .. " origin <branch> eg. (origin main)"
-      end
-      return item
-    end,
-  }, function(choice)
-    if choice == nil then
-      vim.notify("No choice selected", vim.log.levels.WARN)
-      return
+local advanced_opts = {
+  prompt = "Vim-Fugitive options",
+  format_item = function(item)
+    if item == "Push -u" then
+      return item .. " origin <branch> eg. (origin main)"
     end
-    if choice == "Push -u" then
-      local branch = vim.ui.input(
-        { prompt = "Branch to push from: empty = main" },
-        function(input)
-          if input == nil then
-            vim.notify("No input", vim.log.levels.WARN)
-            return nil
-          end
-          if input == "" then
-            vim.notify("No input, using default (main)", vim.log.levels.WARN)
-            return "main"
-          end
-          return input
+    return item
+  end,
+}
+local callback = function(choice)
+  if choice == nil then
+    vim.notify("No choice selected", vim.log.levels.WARN)
+    return
+  end
+  if choice == "Push -u" then
+    vim.ui.input(
+      { prompt = "Branch to push from: empty = main" },
+      function(input)
+        if input == nil then
+          vim.notify("No input", vim.log.levels.WARN)
+          return nil
         end
-      )
-      if branch ~= nil then
-        vim.cmd("Git push origin " .. branch)
+        if input == "" then
+          vim.notify("No input, using default (main)", vim.log.levels.WARN)
+          vim.cmd "Git push origin main"
+        else
+          vim.cmd "Git push"
+        end
       end
-    elseif choice == "Fetch" then
-      vim.cmd "Git fetch --all -p"
-    else
-      vim.cmd("Git " .. choice:lower())
-    end
-  end)
+    )
+  elseif choice == "Fetch" then
+    vim.cmd "Git fetch --all -p"
+  else
+    vim.cmd("Git " .. choice:lower())
+  end
+end
+
+local fugitive_select = function()
+  vim.ui.select(select_opts, advanced_opts, callback)
 end
 
 local opts = { noremap = true, silent = true, buffer = 0 }
 local mappings = {
   n = {
     ["<leader>go"] = {
-      select,
+      fugitive_select,
       "[Fugitive] Git options",
     },
   },
