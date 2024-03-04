767673ab8f61d4bd97506e17a0ad63329d47385f
Alejandro Suero <aome@Alejandros-MacBook-Pro.local>
2024-03-04 20:21:40 +0100

    feat(nvim): Fugitive select options

---
+++ nvim/after/ftplugin/fugitive.lua
@@ -0,0 +1,33 @@
+local select_opts = { "Commit", "Pull", "Push", "Push -u", "Fetch" }
+local select = function()
+  vim.ui.select(select_opts, {
+    prompt = "Vim-Fugitive options",
+    format_item = function(item)
+      if item == "Push -u" then
+        return item .. " origin <branch> eg. (origin main)"
+      end
+      return item
+    end,
+  }, function(choice)
+    if choice == "Push -u" then
+      local branch = vim.fn.input "Branch to push from -> "
+      vim.cmd("Git push origin " .. branch)
+    elseif choice == "Fetch" then
+      vim.cmd "Git fetch --all -p"
+    else
+      vim.cmd("Git " .. choice:lower())
+    end
+  end)
+end
+
+local opts = { noremap = true, silent = true }
+local mappings = {
+  n = {
+    ["<leader>go"] = {
+      select,
+      "[Fugitive] Git options",
+    },
+  },
+}
+
+require("aome.core.utils").map_keys(mappings, opts)
