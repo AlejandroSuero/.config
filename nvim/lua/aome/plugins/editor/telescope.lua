5f066c0d5349d6de02e420598b742b0101e13e73
Alejandro Suero <asuerome@gmail.com>
2024-03-05 12:48:37 +0100

    feat(nvim): Added fzf-native support for Windows

--- nvim/lua/aome/plugins/editor/telescope.lua
+++ nvim/lua/aome/plugins/editor/telescope.lua
@@ -1,3 +1,10 @@
+local executable = "make"
+local build_command = executable
+if vim.fn.has "win32" == 1 then
+  executable = "cmake"
+  build_command = executable
+    .. " -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+end
 return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
@@ -9,12 +16,12 @@ return {
 
       -- `build` is used to run some command when the plugin is installed/updated.
       -- This is only run then, not every time Neovim starts up.
-      build = "make",
+      build = build_command,
 
       -- `cond` is a condition used to determine whether this plugin should be
       -- installed and loaded.
       cond = function()
-        return vim.fn.executable "make" == 1 and vim.fn.has "win32" == 0
+        return vim.fn.executable(executable) == 1
       end,
     },
   },
