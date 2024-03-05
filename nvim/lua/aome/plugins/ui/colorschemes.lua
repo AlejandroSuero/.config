4b402f579fb93e2b71dfb1a6b64f00ab0381428f
Alejandro Suero <asuerome@gmail.com>
2024-03-05 19:05:08 +0100

    feat(nvim): Added rose-pine colorscheme

--- nvim/lua/aome/plugins/ui/colorschemes.lua
+++ nvim/lua/aome/plugins/ui/colorschemes.lua
@@ -37,4 +37,11 @@ return {
     priority = 1000,
     opts = require(configs .. "tokyonight"),
   },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
+    name = "rose-pine",
+    opts = require(configs .. "rose-pine"),
+  },
 }
